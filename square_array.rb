656fb9408bdae15ab80d9ad9e099f67252fa3fdc
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-20 19:34:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   numbers = [1,2,3]
   count = 1
-  square_array(numbers).each do |number|
+  numbers.each do |number|
     squ = number*number
     count += 1
     puts squ
